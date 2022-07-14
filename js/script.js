659bcc0804295ca49dde6c5535b254b300db391f
Rafael Valquer Moraes dos Santos <rafael.santos@dnkinfotelecom.com.br>
2022-07-14 14:47:55 -0300

    melhor

--- js/script.js
+++ js/script.js
@@ -22,6 +22,12 @@
 
             mario.style.animation = 'none';
             mario.style.bottom = `${marioPosition}px`;
+
+            mario.src = './images/game-over.png';
+            mario.style.width = '75px';
+            mario.style.marginLeft = '50px';
+
+            clearInterval(loop);
         }
     }, 10);
 
