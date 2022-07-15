1a29dfc96fc7ac64608c55d3ed09b41618dc93ed
Rafael Valquer Moraes dos Santos <rafael.santos@dnkinfotelecom.com.br>
2022-07-15 14:24:04 -0300

    atualizacao

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
     const mario = document.querySelector('.mario');
     const pipe = document.querySelector('.pipe');
-    
+    const p$ = document.getElementById('contador');
+    const audioStart = new Audio('./audio/theme.mp3')
+    const audioGameOver = new Audio('./audio/gameover.mp3')
+
+
+    document.getElementById('contador').style.color = "rgb(236, 236, 236)";
+
+    let contador = 0;
+
+    p$.innerHTML = contador;
+
     const jump = () => {
         mario.classList.add('jump');
 
@@ -9,11 +19,12 @@
         }, 500);
     }
 
+
     const loop = setInterval(() => {
-        
+
         const pipePosition = pipe.offsetLeft;
         const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
-        console.log ('teste');
+        
 
         if (pipePosition <= 120 && marioPosition < 80 && pipePosition > 0) {
 
@@ -31,4 +42,25 @@
         }
     }, 10);
 
-document.addEventListener('keydown', jump)
+    
+    document.addEventListener('keydown', jump)
+
+    
+    audioStart.play();
+    audioStart.loop =true;
+    audioStart.playbackRate = 2;
+
+
+
+
+
+
+    //function incrementar() {
+    //   document.getElementById("contador").innerHTML = `Value: ${++contador}`;
+    //}
+
+    //function UmSegundoeMeio(){
+      //  contador++;
+        //console.log(contador)
+    //}
+    //setInterval(UmSegundoeMeio, 1000*1.5);
