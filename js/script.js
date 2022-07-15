88ea5c3e7f75bf2d674fffd203c38ac640617302
Rafael Valquer Moraes dos Santos <rafael.santos@dnkinfotelecom.com.br>
2022-07-15 17:15:53 -0300

    1.0.2

--- js/script.js
+++ js/script.js
@@ -2,11 +2,12 @@
     const pipe = document.querySelector('.pipe');
     const grass = document.querySelector('.grass');
     const p$ = document.getElementById('contador');
+    const floor1 = document.querySelector('.floor-1');
+    const floor2 = document.querySelector('.floor-2');
+    const floor3 = document.querySelector('.floor-3');
     const audioStart = new Audio('./audio/theme.mp3');
-    const audioGameOver = new Audio('./audio/gameover.mp3')
-    const floor1 = document.querySelector('.floor-1')
-    const floor2 = document.querySelector('.floor-2')
-    const floor3 = document.querySelector('.floor-3')
+    const audioGameOver = new Audio('./audio/gameover.mp3');
+
 
 
     document.getElementById('contador').style.color = "rgb(236, 236, 236)";
@@ -28,6 +29,11 @@
 
         const pipePosition = pipe.offsetLeft;
         const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+        const grassPosition = grass.offsetLeft;
+        const floorPosition1 = floor1.offsetLeft;
+        const floorPosition2 = floor2.offsetLeft;
+        const floorPosition3 = floor3.offsetLeft;
+        
         
         function grassAnimation(){
             grass.classList.add('grass-animation');
@@ -48,7 +54,7 @@
             floor3.classList.add('floor-animation-3');
                 }setInterval(floorAnimation3, 3100); 
 
-                
+
         audioStart.play();
                 
         if (pipePosition <= 120 && marioPosition < 80 && pipePosition > 0) {
@@ -62,6 +68,23 @@
             mario.src = './images/game-over.webp';
             mario.style.width = '75px';
             mario.style.marginLeft = '50px';
+
+            grass.style.animation = 'none';
+            grass.style.left = `${grassPosition}px`;
+
+            floor1.style.animation = 'none';
+            floor1.style.left = `${floorPosition1}px`;
+
+            floor2.style.animation = 'none';
+            floor2.style.left = `${floorPosition2}px`;
+
+            floor3.style.animation = 'none';
+            floor3.style.left = `${floorPosition3}px`;
+
+
+            audioStart.pause();
+
+            audioGameOver.play();
 
             clearInterval(loop);
         }
